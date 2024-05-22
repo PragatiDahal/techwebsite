456895d7c06fff8ff61f3f7479da786179f62efe
PragatiDahal <pragatidahal2058@gmail.com>
2024-05-22 14:31:02 +0545

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+
+import Navbar from "./Component/Navbar";
+
+export default function App() {
+  return (
+    <Navbar />
+  )
+}
+
---
+++ src/Component/Navbar.tsx
@@ -0,0 +1,30 @@
+
+import photo1 from "../images/Antarikshya website logo.png";
+export default function Navbar() {
+  return (
+    <section>
+        <div className="flex bg-[#023038] w-auto h-16 text-bold text-white font-'century gothic'items-center content-center space-x-5 ">
+            <div className="flex text-2xl text-white font-sans px-8 pt-4 ">
+                SERVICES
+            </div>
+            <div className="flex text-2xl text-white font-sans pt-4 px-8">
+                PROJECTS
+            </div>
+            <div className="flex text-2xl text-white font-sans pt-4 px-8">
+                ABOUT
+            </div>
+            <div className="flex justify-center items-center content-center h-auto w-auto px-8 pl-11 ">
+             <img className="h-[200px] w-[200px] ml-11 pl-11 " src={photo1} alt="logo" />
+          </div>
+          <div className="flex justify-center items-center content-center text-3xl font-sans px-5 text-white">
+                ANTARIKSHYA
+          </div>
+          <button className="flex justify-center items-center rounded-lg border-4 bg-[#023038] h-[50px] w-[150px] border-black pt-4 pl-11 p-4 px-4">
+
+
+          </button>
+        </div>
+    </section>
+  )
+}
+
